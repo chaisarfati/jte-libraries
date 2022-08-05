f7734dff40608622ae83c143fa27d5f383f6d6c2
Chai Sarfati <cimorsar@protonmail.com>
2022-08-05 16:00:57 +0300

    added libraries for maven and sonarqube

---
+++ maven/steps/build.groovy
@@ -0,0 +1,5 @@
+void call(){
+    stage("Maven: Build"){
+        println "TODO Implement here a maven build"
+    }
+}
---
+++ sonarqube/steps/static_code_analysis.groovy
@@ -0,0 +1,3 @@
+void call(){
+    println "TODO implement here sonarqube static code analysis"
+}
