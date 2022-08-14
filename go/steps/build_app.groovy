35e0501213ec760aafad4ad24f10c17b4a094a5a
chaisarfati <chaisarfati@gmail.com>
2022-08-14 17:12:31 +0300

    Update build_app.groovy

--- go/steps/build_app.groovy
+++ go/steps/build_app.groovy
@@ -1,8 +1,10 @@
 void call(){
     stage("Go: Build"){
         println "TODO Implement here a go build"
+        sh("ls")
     }
     stage("Go: Test"){
         println "TODO Implement here a go test"
+        sh("ls")
     }
 }
