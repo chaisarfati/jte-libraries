3f53ac44c6a2412e8dee74e836f252b0c7367f9c
chaisarfati <chaisarfati@gmail.com>
2022-08-14 18:10:52 +0300

    Update build_app.groovy

--- go/steps/build_app.groovy
+++ go/steps/build_app.groovy
@@ -1,7 +1,12 @@
 void call(){
     stage("Go: Build"){
-        println "TODO Implement here a go build"
+        println "Listing directory content"
         sh("ls")
+        String uid = UUID.randomUUID().toString()
+	    def label = "slave-${uid}"
+        node(label){
+            echo "hi , from node"
+        }
         println "Printing config variable"
         println config
     }
