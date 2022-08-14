3571a9d31a84f0eb3e8683f86869caba8e0563d9
chaisarfati <chaisarfati@gmail.com>
2022-08-14 18:22:07 +0300

    Update build_app.groovy

--- go/steps/build_app.groovy
+++ go/steps/build_app.groovy
@@ -2,11 +2,14 @@ void call(){
     stage("Go: Build"){
         println "Listing directory content"
         sh("ls")
-        String uid = UUID.randomUUID().toString()
-	    def label = "slave-${uid}"
-        node(label){
-            echo "hi , from node"
-        }
+        node {
+    		checkout scm
+    		docker.image('golang:1.19.0-alpine3.16') { c ->
+			sh 'ls' 
+		}
+	}
+
+
         println "Printing config variable"
         println config
     }
