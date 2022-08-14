a99b063b78996a502d0d3c6819812f959b24b019
chaisarfati <chaisarfati@gmail.com>
2022-08-14 18:44:08 +0300

    Update build_app.groovy

--- go/steps/build_app.groovy
+++ go/steps/build_app.groovy
@@ -4,9 +4,17 @@ void call(){
         sh("ls")
         node {
     		checkout scm
-    		docker.image('golang:1.19.0-alpine3.16').withRun() { c ->
-			sh 'ls' 
-		}
+//     		docker.image('golang:1.19.0-alpine3.16').withRun() { c ->
+// 			sh 'ls' 
+// 		}
+		docker.image('mysql:5').withRun('-e "MYSQL_ROOT_PASSWORD=my-secret-pw"' +
+                                    ' -p 3306:3306') { c ->
+        /* Wait until mysql service is up */
+        sh 'while ! mysqladmin ping -h0.0.0.0 --silent; do sleep 1; done'
+        /* Run some tests which require MySQL */
+	echo 'inside mysql image'
+        sh 'ls'
+    }
 	}
 
 
