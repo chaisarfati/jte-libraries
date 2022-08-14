76a06a4842a7f6768f7903959736268c434a304f
chaisarfati <chaisarfati@gmail.com>
2022-08-14 17:38:50 +0300

    Update build_app.groovy

--- go/steps/build_app.groovy
+++ go/steps/build_app.groovy
@@ -2,6 +2,8 @@ void call(){
     stage("Go: Build"){
         println "TODO Implement here a go build"
         sh("ls")
+        println "Printing config variable"
+        println config
     }
     stage("Go: Test"){
         println "TODO Implement here a go test"
