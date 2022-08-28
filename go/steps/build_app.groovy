fd95658cce6888d58fe20f523ec94a2af7fb1796
chaisarfati <chaisarfati@gmail.com>
2022-08-28 14:14:35 +0300

    Update build_app.groovy

--- go/steps/build_app.groovy
+++ go/steps/build_app.groovy
@@ -1,28 +1,21 @@
 void call(){
     stage("Go: Build"){
+	println "Printing config variable :\n" + config
         println "Listing directory content"
         sh("ls")
         node {
     		checkout scm
-		echo "hello from node guys !!"
-		
-//     		docker.image('golang:1.19.0-alpine3.16').withRun() { c ->
-// 			sh 'ls' 
-// 		}
-		
-		docker.image('mysql:5').withRun('-e "MYSQL_ROOT_PASSWORD=my-secret-pw"' +
-                                    ' -p 3306:3306') { c ->
-        		sh 'echo "hi"'
-       	 		sh 'ls'
-    		}
+		echo "Before running go build command"
+		sh("go build")
+		echo "After running go build command"
 	}
 
+	println "Listing directory content"
+	sh("ls")
 
-        println "Printing config variable"
-        println config
     }
+	
     stage("Go: Test"){
-        println "TODO Implement here a go test"
-        sh("ls")
+        sh("go test")
     }
 }
