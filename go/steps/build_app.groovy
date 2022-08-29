8facff75c925473c3573e8e7676bd95b880b2019
chaisarfati <chaisarfati@gmail.com>
2022-08-29 19:40:50 +0300

    Update build_app.groovy

--- go/steps/build_app.groovy
+++ go/steps/build_app.groovy
@@ -1,27 +1,27 @@
 void call(){
+	
+    checkout scm
+    ls()
+	
     stage("Go: Build"){
-	println "Printing config variable :\n" + config
-        println "Listing directory content"
-        sh("ls")
     	withDockerContainer(args:'-e GOCACHE=/tmp/' ,image: 'golang:latest'){
-		println "Listing directory content in container"
-		sh("ls")
-        	echo "Before running go build command"
+        	echo "Running go build command"
 		sh("go build")
     	}
-	sh("ls")
     }
 	
     stage("Go: Test"){
-	println "Listing directory content before checkout"
-        sh("ls")
-	checkout scm
-	println "Listing directory content after checkout"
-        sh("ls")
 
     	withDockerContainer(args:'-e GOCACHE=/tmp/' ,image: 'golang:latest'){
         	echo "Running go test command"
 		sh("go test")
     	}
     }
+
+    ls()
+}
+
+void ls() {
+	echo "Listing current directory content"
+	sh("ls")
 }
