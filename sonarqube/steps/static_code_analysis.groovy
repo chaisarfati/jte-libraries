a837410a86ec6864a9a29ac7120b418f5c747720
chaisarfati <chaisarfati@gmail.com>
2022-09-09 13:08:25 +0300

    Update static_code_analysis.groovy

--- sonarqube/steps/static_code_analysis.groovy
+++ sonarqube/steps/static_code_analysis.groovy
@@ -1,17 +1,19 @@
 void call(){
-    stage("SonarQube Analysis"){
-        checkout scm
-        
-        echo "creating file sonar-project.properties"
-        sh("echo sonar.projectKey=${config.projectName} > sonar-project.properties")
-        sh("echo sonar.login=${config.token} >> sonar-project.properties")
+    
+    echo "creating file sonar-project.properties"
+    sh("echo sonar.projectKey=${config.projectKey} > sonar-project.properties")
+    sh("echo sonar.login=${config.token} >> sonar-project.properties")
 
-        sh("ls")
-        echo "Starting sonarqube static code analysis"
+    stage("SonarQube Analysis"){
+        echo "Starting sonarqube static code analysis"  
         def scannerHome = tool 'SonarScanner';
         echo "scannerHome = ${scannerHome}"
         withSonarQubeEnv() {
-            sh "${scannerHome}/bin/sonar-scanner"
+            if(config.isJavaProject){
+                sh("mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar")
+            } else {
+                sh("${scannerHome}/bin/sonar-scanner")
+            }
         }
     }
 }
