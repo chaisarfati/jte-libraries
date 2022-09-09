2bed20d1f3b5ee42c2877ac03412fd34181f64d3
chaisarfati <chaisarfati@gmail.com>
2022-09-09 10:07:48 +0300

    Update static_code_analysis.groovy

--- sonarqube/steps/static_code_analysis.groovy
+++ sonarqube/steps/static_code_analysis.groovy
@@ -3,8 +3,8 @@ void call(){
         checkout scm
         
         echo "creating file sonar-project.properties"
-        sh("echo sonar.projectKey=vindecodex-msgo > sonar-project.properties")
-        sh("echo sonar.login=sqp_084b1eb182741292fcdb60e97411edef440f29cb >> sonar-project.properties")
+        sh("echo sonar.projectKey=${config.projectName} > sonar-project.properties")
+        sh("echo sonar.login=${config.token} >> sonar-project.properties")
 
         sh("ls")
         echo "Starting sonarqube static code analysis"
