27bdec5381c3b28122c6a495473c7e5e010e09a0
chaisarfati <chaisarfati@gmail.com>
2022-09-10 21:21:05 +0300

    Update static_code_analysis.groovy

--- sonarqube/steps/static_code_analysis.groovy
+++ sonarqube/steps/static_code_analysis.groovy
@@ -10,17 +10,7 @@ void call(){
         echo "scannerHome = ${scannerHome}"
         withSonarQubeEnv() {
             if(config.isJavaProject){
-                sh("echo sonar.sources=src/main >> sonar-project.properties")
-                sh("echo sonar.sourceEncoding=UTF-8 >> sonar-project.properties")
-                sh("echo sonar.language=java >> sonar-project.properties")
-                sh("echo sonar.tests=src/test >> sonar-project.properties")
-                sh("echo sonar.junit.reportsPath=target/surefire-reports >> sonar-project.properties")
-                sh("echo sonar.surefire.reportsPath=target/surefire-reports >> sonar-project.properties")
-                sh("echo sonar.jacoco.reportPath=target/jacoco.exec >> sonar-project.properties")
-                sh("echo sonar.java.coveragePlugin=jacoco >> sonar-project.properties")
-                                sh("${scannerHome}/bin/sonar-scanner")
-
-                //sh("mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar")
+                sh("mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar")
             } else {
                 sh("${scannerHome}/bin/sonar-scanner")
             }
