eb8db72356b0aa1de0ec3fbe59012eba35148f79
chaisarfati <chaisarfati@gmail.com>
2022-09-12 15:22:42 +0300

    Update static_code_analysis.groovy

--- sonarqube/steps/static_code_analysis.groovy
+++ sonarqube/steps/static_code_analysis.groovy
@@ -10,7 +10,9 @@ void call(){
         echo "scannerHome = ${scannerHome}"
         withSonarQubeEnv() {
             if(config.isJavaProject){
-                sh("mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar")
+                withDockerContainer("maven:3.5.0-jdk-8-alpine") { 
+                    sh("mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar")
+                }
             } else {
                 sh("${scannerHome}/bin/sonar-scanner")
             }
