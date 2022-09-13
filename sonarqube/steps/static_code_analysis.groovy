35473fb72929a57d72f6868e12983a4296b79bca
chaisarfati <chaisarfati@gmail.com>
2022-09-13 20:26:00 +0300

    Update static_code_analysis.groovy

--- sonarqube/steps/static_code_analysis.groovy
+++ sonarqube/steps/static_code_analysis.groovy
@@ -1,20 +1,21 @@
 void call(){
     
-    echo "creating file sonar-project.properties"
-    sh("echo sonar.projectKey=${config.projectKey} > sonar-project.properties")
-    sh("echo sonar.login=${config.token} >> sonar-project.properties")
-
     stage("SonarQube Analysis"){
         echo "Starting sonarqube static code analysis"  
         def scannerHome = tool 'SonarScanner';
         echo "scannerHome = ${scannerHome}"
-        withSonarQubeEnv() {
-            if(config.isJavaProject){
-                withDockerContainer(args:'--network=sonarqube_default', image:"maven:3.8.6-jdk-11-slim") { 
-                    sh("mvn org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -X -Dsonar.projectKey=${config.projectKey} -Dsonar.login=${config.token}")
+        withCredentials([string(credentialsId: 'sonarqube_token', variable: 'TOKEN')]){
+            withSonarQubeEnv() {
+                if(config.isJavaProject){
+                    withDockerContainer(args:'--network=sonarqube_default', image:"maven:3.8.6-jdk-11-slim") { 
+                        sh("mvn org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -X -Dsonar.projectKey=${config.projectKey} -Dsonar.login=$TOKEN")
+                    }
+                } else {
+                    echo "creating file sonar-project.properties"
+                    sh("echo sonar.projectKey=${config.projectKey} > sonar-project.properties")
+                    sh("echo sonar.login=$TOKEN >> sonar-project.properties")
+                    sh("${scannerHome}/bin/sonar-scanner")
                 }
-            } else {
-                sh("${scannerHome}/bin/sonar-scanner")
             }
         }
     }
