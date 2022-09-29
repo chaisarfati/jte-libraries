0c02f5ca87c5eaac75dbae640a20ade79601d204
chaisarfati <chaisarfati@gmail.com>
2022-09-30 00:03:01 +0300

    Update containerize.groovy

--- docker/steps/containerize.groovy
+++ docker/steps/containerize.groovy
@@ -1,16 +1,12 @@
 void call(){
     stage("Docker: Containerization"){        
         def imageName = config?.imageName ? config.imageName : env.JOB_NAME
-        echo "imageName = ${imageName}"
-        def pushedImageFullName = imageName + ((env.BRANCH_NAME == "master") ? "" : "${env.BRANCH_NAME}-") + env.BUILD_ID
-        echo "pushedImageFullName = ${pushedImageFullName}"
+        def taggedImage = imageName + ((env.BRANCH_NAME == "master") ? ":" : "${env.BRANCH_NAME}:") + env.BUILD_ID
         def dockerFilePath = config.dockerFilePath ? config.dockerFilePath : "."
-        echo "dockerFilePath = ${dockerFilePath}"
         
         
-        sh "docker build ${dockerFilePath} -t ${pushedImageFullName}"
-        sh "docker tag ${pushedImageFullName} registry:5000/${pushedImageFullName}"
-        sh "docker push registry:5000/${pushedImageFullName}"
-
+        sh "docker build ${dockerFilePath} -t ${taggedImage}"
+        sh "docker tag ${taggedImage} registry:5000/${taggedImage}"
+        sh "docker push registry:5000/${taggedImage}"
     }
 }
